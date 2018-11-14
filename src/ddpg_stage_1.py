2169d32a04aaff4f0278ba3e06297744e2c12b58
Junior Costa de Jesus <dranaju@gmail.com>
2018-11-14 19:53:17 +0000

    first version

---
+++ src/ddpg_stage_1.py
@@ -0,0 +1,377 @@
+#!/usr/bin/env python
+# Authors: Junior Costa de Jesus #
+
+import rospy
+import os
+import json
+import numpy as np
+import random
+import time
+import sys
+sys.path.append(os.path.dirname(os.path.abspath(os.path.dirname(__file__))))
+from collections import deque
+from std_msgs.msg import Float32
+from environment_stage_1 import Env
+import torch
+import torch.nn.functional as F
+import gc
+import torch.nn as nn
+import math
+from collections import deque
+
+#---Directory Path---#
+dirPath = os.path.dirname(os.path.realpath(__file__))
+#---Functions to make network updates---#
+ 
+def soft_update(target, source, tau):
+    for target_param, param in zip(target.parameters(), source.parameters()):
+        target_param.data.copy_(target_param.data*(1.0 - tau)+ param.data*tau)
+
+def hard_update(target,source):
+    for target_param, param in zip(target.parameters(), source.parameters()):
+        target_param.data.copy_(param.data)
+
+#---Ornstein-Uhlenbeck Noise for action---#
+
+class ActionNoise:
+    # Based on http://math.stackexchange.com/questions/1287634/implementing-ornstein-uhlenbeck-in-matlab
+    def __init__(self, action_dim, mu=0, theta=0.15, sigma=0.2):
+        self.action_dim = action_dim
+        self.mu = mu
+        self.theta = theta
+        self.sigma = sigma
+        self.X = np.ones(self.action_dim)*self.mu
+        
+    def reset(self):
+        self.X = np.ones(self.action_dim)*self.mu
+    
+    def sample(self):
+        dx = self.theta*(self.mu - self.X)
+        dx = dx + self.sigma*np.random.randn(len(self.X))
+        self.X = self.X + dx
+        print('aqu2i' + str(self.X))
+        return self.X
+
+#---Critic--#
+
+EPS = 0.003
+def fanin_init(size, fanin=None):
+    fanin = fanin or size[0]
+    v = 1./np.sqrt(fanin)
+    return torch.Tensor(size).uniform_(-v,v)
+
+class Critic(nn.Module):
+    def __init__(self, state_dim, action_dim):
+        super(Critic, self).__init__()
+        
+        self.state_dim = state_dim = state_dim
+        self.action_dim = action_dim
+        
+        self.fc1 = nn.Linear(state_dim, 256)
+        self.fc1.weight.data = fanin_init(self.fc1.weight.data.size())
+        
+        self.fa1 = nn.Linear(action_dim, 256)
+        self.fa1.weight.data = fanin_init(self.fa1.weight.data.size())
+        
+        self.fca1 = nn.Linear(512, 512)
+        self.fca1.weight.data = fanin_init(self.fca1.weight.data.size())
+        
+        self.fca2 = nn.Linear(512, 1)
+        self.fca2.weight.data.uniform_(-EPS, EPS)
+        
+    def forward(self, state, action):
+        xs = torch.relu(self.fc1(state))
+        xa = torch.relu(self.fa1(action))
+        x = torch.cat((xs,xa), dim=1)
+        x = torch.relu(self.fca1(x))
+        vs = self.fca2(x)
+        return vs
+
+#---Actor---#
+
+class Actor(nn.Module):
+    def __init__(self, state_dim, action_dim, action_limit_v, action_limit_w):
+        super(Actor, self).__init__()
+        self.state_dim = state_dim = state_dim
+        self.action_dim = action_dim
+        self.action_limit_v = action_limit_v
+        self.action_limit_w = action_limit_w
+        
+        self.fa1 = nn.Linear(state_dim, 512)
+        self.fa1.weight.data = fanin_init(self.fa1.weight.data.size())
+        
+        self.fa2 = nn.Linear(512, 512)
+        self.fa2.weight.data = fanin_init(self.fa2.weight.data.size())
+        
+        self.fa3 = nn.Linear(512, action_dim)
+        self.fa3.weight.data.uniform_(-EPS,EPS)
+        
+    def forward(self, state):
+        x = torch.relu(self.fa1(state))
+        x = torch.relu(self.fa2(x))
+        action = self.fa3(x)
+        if state.shape == torch.Size([14]):
+            action[0] = torch.sigmoid(action[0])*self.action_limit_v
+            action[1] = torch.tanh(action[1])*self.action_limit_w
+        else:
+            action[:,0] = torch.sigmoid(action[:,0])*self.action_limit_v
+            action[:,1] = torch.tanh(action[:,1])*self.action_limit_w
+        return action
+
+#---Memory Buffer---#
+
+class MemoryBuffer:
+    def __init__(self, size):
+        self.buffer = deque(maxlen=size)
+        self.maxSize = size
+        self.len = 0
+        
+    def sample(self, count):
+        batch = []
+        count = min(count, self.len)
+        batch = random.sample(self.buffer, count)
+        
+        s_array = np.float32([array[0] for array in batch])
+        a_array = np.float32([array[1] for array in batch])
+        r_array = np.float32([array[2] for array in batch])
+        new_s_array = np.float32([array[3] for array in batch])
+        
+        return s_array, a_array, r_array, new_s_array
+    
+    def len(self):
+        return self.len
+    
+    def add(self, s, a, r, new_s):
+        transition = (s, a, r, new_s)
+        self.len += 1 
+        if self.len > self.maxSize:
+            self.len = self.maxSize
+        self.buffer.append(transition)
+
+#---Where the train is made---#
+
+BATCH_SIZE = 128
+LEARNING_RATE = 0.001
+GAMMA = 0.99
+TAU = 0.001
+
+class Trainer:
+    
+    def __init__(self, state_dim, action_dim, action_limit_v, action_limit_w, ram):
+        
+        self.state_dim = state_dim
+        self.action_dim = action_dim
+        self.action_limit_v = action_limit_v
+        self.action_limit_w = action_limit_w
+        #print('w',self.action_limit_w)
+        self.ram = ram
+        #self.iter = 0 
+        self.noise = ActionNoise(self.action_dim)
+        
+        self.actor = Actor(self.state_dim, self.action_dim, self.action_limit_v, self.action_limit_w)
+        self.target_actor = Actor(self.state_dim, self.action_dim, self.action_limit_v, self.action_limit_w)
+        self.actor_optimizer = torch.optim.Adam(self.actor.parameters(), LEARNING_RATE)
+        
+        self.critic = Critic(self.state_dim, self.action_dim)
+        self.target_critic = Critic(self.state_dim, self.action_dim)
+        self.critic_optimizer = torch.optim.Adam(self.critic.parameters(), LEARNING_RATE)
+        self.pub_qvalue = rospy.Publisher('qvalue', Float32, queue_size=5)
+        self.qvalue = Float32()
+        
+        hard_update(self.target_actor, self.actor)
+        hard_update(self.target_critic, self.critic)
+        
+    def get_exploitation_action(self,state):
+        state = torch.from_numpy(state)
+        action = self.target_actor.forward(state).detach()
+        #print('actionploi', action)
+        return action.data.numpy()
+        
+    def get_exploration_action(self, state):
+        state = torch.from_numpy(state)
+        action = self.actor.forward(state).detach()
+        #noise = self.noise.sample()
+        #print('noisea', noise)
+        #noise[0] = noise[0]*self.action_limit_v
+        #noise[1] = noise[1]*self.action_limit_w
+        #print('noise', noise)
+        new_action = action.data.numpy() #+ noise
+        #print('action_no', new_action)
+        return new_action
+    
+    def optimizer(self):
+        s_sample, a_sample, r_sample, new_s_sample = ram.sample(BATCH_SIZE)
+        
+        s_sample = torch.from_numpy(s_sample)
+        a_sample = torch.from_numpy(a_sample)
+        r_sample = torch.from_numpy(r_sample)
+        new_s_sample = torch.from_numpy(new_s_sample)
+        
+        #-------------- optimize critic
+        
+        a_target = self.target_actor.forward(new_s_sample).detach()
+        next_value = torch.squeeze(self.target_critic.forward(new_s_sample, a_target).detach())
+        # y_exp = r _ gamma*Q'(s', P'(s'))
+        y_expected = r_sample + GAMMA*next_value
+        # y_pred = Q(s,a)
+        y_predicted = torch.squeeze(self.critic.forward(s_sample, a_sample))
+        #-------Publisher of Vs------
+        self.qvalue = y_predicted.detach()
+        self.pub_qvalue.publish(torch.max(self.qvalue))
+        #print(self.qvalue, torch.max(self.qvalue))
+        #----------------------------
+        loss_critic = F.smooth_l1_loss(y_predicted, y_expected)
+        
+        self.critic_optimizer.zero_grad()
+        loss_critic.backward()
+        self.critic_optimizer.step()
+        
+        #------------ optimize actor
+        pred_a_sample = self.actor.forward(s_sample)
+        loss_actor = -1*torch.sum(self.critic.forward(s_sample, pred_a_sample))
+        
+        self.actor_optimizer.zero_grad()
+        loss_actor.backward()
+        self.actor_optimizer.step()
+        
+        soft_update(self.target_actor, self.actor, TAU)
+        soft_update(self.target_critic, self.critic, TAU)
+    
+    def save_models(self, episode_count):
+        torch.save(self.target_actor.state_dict(), dirPath +'/Models/stage_4/'+str(episode_count)+ '_actor1.pt')
+        torch.save(self.target_critic.state_dict(), dirPath + '/Models/stage_4/'+str(episode_count)+ '_critic1.pt')
+        print('****Models saved***')
+        
+    def load_models(self, episode):
+        self.actor.load_state_dict(torch.load(dirPath + '/Models/stage_4/'+str(episode)+ '_actor1.pt'))
+        self.critic.load_state_dict(torch.load(dirPath + '/Models/stage_4/'+str(episode)+ '_critic1.pt'))
+        hard_update(self.target_actor, self.actor)
+        hard_update(self.target_critic, self.critic)
+        print('***Models load***')
+
+#---Run agent---#
+
+is_training = True
+
+if is_training:
+    exploration_rate = 1
+    max_exploration_rate = 1
+    min_exploration_rate = 0.05
+else:
+    exploration_rate = 0.05
+    max_exploration_rate = 0.05
+    min_exploration_rate = 0.05
+
+exploration_decay_rate = 0.001
+
+MAX_EPISODES = 10001
+MAX_STEPS = 400
+MAX_BUFFER = 100000
+rewards_all_episodes = []
+
+STATE_DIMENSION = 14
+ACTION_DIMENSION = 2
+ACTION_V_MAX = 0.22 # m/s
+ACTION_W_MAX = 0.5 # rad/s
+
+if is_training:
+    var_v = ACTION_V_MAX
+    var_w = ACTION_W_MAX*2
+else:
+    var_v = ACTION_V_MAX*0.10
+    var_w = ACTION_W_MAX*0.10
+
+print('State Dimensions: ' + str(STATE_DIMENSION))
+print('Action Dimensions: ' + str(ACTION_DIMENSION))
+print('Action Max: ' + str(ACTION_V_MAX) + ' m/s and ' + str(ACTION_W_MAX) + ' rad/s')
+ram = MemoryBuffer(MAX_BUFFER)
+trainer = Trainer(STATE_DIMENSION, ACTION_DIMENSION, ACTION_V_MAX, ACTION_W_MAX, ram)
+trainer.load_models(1150)
+
+
+if __name__ == '__main__':
+    rospy.init_node('ddpg_stage_1')
+    pub_result = rospy.Publisher('result', Float32, queue_size=5)
+    result = Float32()
+    env = Env()
+
+    start_time = time.time()
+    past_action = np.array([0.,0.])
+
+    for ep in range(MAX_EPISODES):
+        done = False
+        state = env.reset()
+        print('Episode: ' + str(ep))
+
+        rewards_current_episode = 0
+        for step in range(MAX_STEPS):
+            #print(step)
+            state = np.float32(state)
+            #action = trainer.get_exploration_action(state)
+            #print('actionA:', action)
+            
+            #action[0] = np.clip(np.random.normal(action[0], var_v), 0., ACTION_V_MAX)
+            #action[1] = np.clip(np.random.normal(action[1], var_w), -ACTION_W_MAX, ACTION_W_MAX)
+            #print('actionD', action)
+
+            if is_training:
+                #if ep%2 == 0:
+                #    action = trainer.get_exploitation_action(state)
+                #    #print('aa', action)
+                #else:
+                action = trainer.get_exploration_action(state)
+                #    #print('aa',action)
+                action[0] = np.clip(np.random.normal(action[0], var_v), 0., ACTION_V_MAX)
+                action[1] = np.clip(np.random.normal(action[1], var_w), -ACTION_W_MAX, ACTION_W_MAX)
+                #action[0] = np.clip(action[0], 0., ACTION_V_MAX)
+                #action[1] = np.clip(action[1], -ACTION_W_MAX, ACTION_W_MAX)
+            #print('af', action) 
+
+            #exploration_rate_threshold = random.uniform(0., 1.)
+            #if exploration_rate_threshold > exploration_rate:
+            #    action = trainer.get_exploration_action(state)
+            #else:
+            #    action = np.array([np.random.uniform(0,0.22), np.random.uniform(-1,1)])
+
+            if not is_training:
+                #print('nor')
+                action = trainer.get_exploitation_action(state)
+            #print('state',state)
+            #print('action',action)
+            #print('ap',past_action)
+            next_state, reward, done = env.step(action, past_action)
+            print('action', action,'r',reward)
+            past_action = action
+
+            rewards_current_episode += reward
+            next_state = np.float32(next_state)
+            ram.add(state, action, reward, next_state)
+            state = next_state
+            
+            #action = np.array([np.random.uniform(0.,0.15), np.random.uniform(-0.5, 0.5)])
+            #print('r: ' + str(reward) + ' and done: ' + str(done))
+
+            if ram.len >= 0*MAX_STEPS and is_training:
+                var_v = max([var_v*0.99999, 0.30*ACTION_V_MAX])
+                var_w = max([var_w*0.99999, 0.30*ACTION_W_MAX])
+                trainer.optimizer()
+            #if is_training:
+            #    trainer.optimizer()
+
+            if done or step == MAX_STEPS-1:
+                print('reward per ep: ' + str(rewards_current_episode))
+                print('explore_v: ' + str(var_v) + ' and explore_w: ' + str(var_w))
+                rewards_all_episodes.append(rewards_current_episode)
+                result = rewards_current_episode
+                pub_result.publish(result)
+                m, s = divmod(int(time.time() - start_time), 60)
+                h, m = divmod(m, 60)
+                break
+        exploration_rate = (min_exploration_rate +
+                (max_exploration_rate - min_exploration_rate)* np.exp(-exploration_decay_rate*ep))
+        gc.collect()
+        #print('exp:', exploration_rate)
+        if ep%50 == 0:
+            trainer.save_models(ep)
+
+print('Completed Training')
---
+++ src/environment_stage_1.py
@@ -0,0 +1,175 @@
+#!/usr/bin/env python
+#################################################################################
+# Copyright 2018 ROBOTIS CO., LTD.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#################################################################################
+
+# Authors: Gilbert #
+
+import rospy
+import numpy as np
+import math
+from math import pi
+from geometry_msgs.msg import Twist, Point, Pose
+from sensor_msgs.msg import LaserScan
+from nav_msgs.msg import Odometry
+from std_srvs.srv import Empty
+from tf.transformations import euler_from_quaternion, quaternion_from_euler
+from respawnGoal import Respawn
+
+class Env():
+    def __init__(self):
+        self.goal_x = 0
+        self.goal_y = 0
+        self.heading = 0
+        self.initGoal = True
+        self.get_goalbox = False
+        self.position = Pose()
+        self.pub_cmd_vel = rospy.Publisher('cmd_vel', Twist, queue_size=5)
+        self.sub_odom = rospy.Subscriber('odom', Odometry, self.getOdometry)
+        self.reset_proxy = rospy.ServiceProxy('gazebo/reset_simulation', Empty)
+        self.unpause_proxy = rospy.ServiceProxy('gazebo/unpause_physics', Empty)
+        self.pause_proxy = rospy.ServiceProxy('gazebo/pause_physics', Empty)
+        self.respawn_goal = Respawn()
+        self.past_distance = 0.
+
+    def getGoalDistace(self):
+        goal_distance = round(math.hypot(self.goal_x - self.position.x, self.goal_y - self.position.y), 2)
+        self.past_distance = goal_distance
+
+        return goal_distance
+
+    def getOdometry(self, odom):
+        self.position = odom.pose.pose.position
+        orientation = odom.pose.pose.orientation
+        orientation_list = [orientation.x, orientation.y, orientation.z, orientation.w]
+        _, _, yaw = euler_from_quaternion(orientation_list)
+
+        goal_angle = math.atan2(self.goal_y - self.position.y, self.goal_x - self.position.x)
+
+
+        heading = goal_angle - yaw
+        if heading > pi:
+            heading -= 2 * pi
+
+        elif heading < -pi:
+            heading += 2 * pi
+
+        self.heading = round(heading, 3)
+
+    def getState(self, scan, past_action):
+        scan_range = []
+        heading = self.heading
+        min_range = 0.20
+        done = False
+
+        for i in range(len(scan.ranges)):
+            if scan.ranges[i] == float('Inf'):
+                scan_range.append(3.5)
+            elif np.isnan(scan.ranges[i]):
+                scan_range.append(0)
+            else:
+                scan_range.append(scan.ranges[i])
+
+
+        if min_range > min(scan_range) > 0:
+            done = True
+
+        for pa in past_action:
+            scan_range.append(pa)
+
+        current_distance = round(math.hypot(self.goal_x - self.position.x, self.goal_y - self.position.y),2)
+        if current_distance < 0.2:
+            self.get_goalbox = True
+
+        return scan_range + [heading, current_distance], done
+
+    def setReward(self, state, done):
+        current_distance = state[-1]
+        heading = state[-2]
+        #print('cur:', current_distance, self.past_distance)
+
+
+        distance_rate = (self.past_distance - current_distance) 
+        #if distance_rate > 0:
+        #    reward = 500.*distance_rate
+        #if distance_rate == 0:
+        #    reward = -10.
+        #if distance_rate <= 0:
+        #    reward = -15.
+        #angle_reward = math.pi - abs(heading)
+        #print('d', 500*distance_rate)
+        reward = 500.*distance_rate #+ 3.*angle_reward
+        self.past_distance = current_distance
+
+        if done:
+            rospy.loginfo("Collision!!")
+            reward = -75.
+            self.pub_cmd_vel.publish(Twist())
+
+        if self.get_goalbox:
+            rospy.loginfo("Goal!!")
+            reward = 100.
+            self.pub_cmd_vel.publish(Twist())
+            self.goal_x, self.goal_y = self.respawn_goal.getPosition(True, delete=True)
+            self.goal_distance = self.getGoalDistace()
+            self.get_goalbox = False
+
+        return reward
+
+    def step(self, action, past_action):
+        linear_vel = action[0]
+        ang_vel = action[1]
+
+        vel_cmd = Twist()
+        vel_cmd.linear.x = linear_vel
+        vel_cmd.angular.z = ang_vel
+        self.pub_cmd_vel.publish(vel_cmd)
+
+        data = None
+        while data is None:
+            try:
+                data = rospy.wait_for_message('scan', LaserScan, timeout=5)
+            except:
+                pass
+
+        state, done = self.getState(data, past_action)
+        reward = self.setReward(state, done)
+
+        return np.asarray(state), reward, done
+
+    def reset(self):
+        rospy.wait_for_service('gazebo/reset_simulation')
+        try:
+            self.reset_proxy()
+        except (rospy.ServiceException) as e:
+            print("gazebo/reset_simulation service call failed")
+
+        data = None
+        while data is None:
+            try:
+                data = rospy.wait_for_message('scan', LaserScan, timeout=5)
+            except:
+                pass
+
+        if self.initGoal:
+            self.goal_x, self.goal_y = self.respawn_goal.getPosition()
+            self.initGoal = False
+        else:
+            self.goal_x, self.goal_y = self.respawn_goal.getPosition(True, delete=True)
+
+        self.goal_distance = self.getGoalDistace()
+        state, done = self.getState(data, [0.,0.])
+
+        return np.asarray(state)
